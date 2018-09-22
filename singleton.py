274595782ca1eba477e6ff0c8cd17de060a29db5
MASER <maser.cuhk@gmail.com>
2018-09-22 17:53:56 +0200

    003 Singleton Add test

--- singleton.py
+++ singleton.py
@@ -1,21 +1,38 @@
 class singleton:
-	__instance=None
+	__instance=None 
+	__quantity=None
 
-	@staticmethod
-	def getInstance():
-		if singleton.__instance==None:
-			singleton()
-		return singleton.__instance
-	def __init__(self):
+	def __init__(self,quantity):
 		if singleton.__instance!=None:
-			raise Exception("Singleton Class")
+			print "instance is already created"
 		else:
 			singleton.__instance=self
+			singleton.__quantity=quantity
+		#	print singleton.__quantity
+	
+	@staticmethod
+	def getInstance(): 
+		""" Static access method. """
+		if singleton.__instance == None:
+			singleton(1000)
+		return singleton.__instance 
+	
+	@staticmethod
+	def getQuantity():
+		return singleton.__quantity
+	
+	@staticmethod 	
+	def addQuantity(quantity):
+		singleton.__quantity+=quantity
+	
+	@staticmethod 
+	def delQuantity(quantity):
+		singleton.__quantity-=quantity
 
-s=singleton()
-print s
-s = singleton.getInstance()
-print s
+s=singleton(1000)
+print s.getQuantity() 
+singleton(5000)
+s.addQuantity(1515)
+s.delQuantity(421)
+print s.getQuantity()
 
-s = singleton.getInstance()
-print s
